785ed5c9d1d5295722695e066c66b5fa1872476e
Molly Stone <molly.stone89@gmail.com>
2020-03-13 14:20:17 -0400

    Update product list with async pipe

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,41 +1,32 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
 
 @Component({
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit, OnDestroy {
-  pageTitle = 'Product List';
-  errorMessage = '';
-  categories;
+export class ProductListComponent implements OnInit {
+    pageTitle = 'Product List';
+    errorMessage = '';
+    categories;
 
-  products: Product[] = [];
-  sub: Subscription;
+    products$: Observable<Product[]>;
 
-  constructor(private productService: ProductService) { }
+    constructor(private productService: ProductService) { }
 
-  ngOnInit(): void {
-    this.sub = this.productService.getProducts()
-      .subscribe(
-        products => this.products = products,
-        error => this.errorMessage = error
-      );
-  }
+    ngOnInit(): void {
+        this.products$ = this.productService.getProducts();
+    }
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
+    onAdd(): void {
+        console.log('Not yet implemented');
+    }
 
-  onAdd(): void {
-    console.log('Not yet implemented');
-  }
-
-  onSelected(categoryId: string): void {
-    console.log('Not yet implemented');
-  }
+    onSelected(categoryId: string): void {
+        console.log('Not yet implemented');
+    }
 }
