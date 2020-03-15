ed43c1458c1eb9fb046841d7bb5fb80c25b896d7
Molly Stone <molly.stone89@gmail.com>
2020-03-15 11:16:03 -0400

    Implement catch errors, make components more declartive

--- APM/src/app/products/product-list-alt/product-list-alt.component.ts
+++ APM/src/app/products/product-list-alt/product-list-alt.component.ts
@@ -1,36 +1,30 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { Subscription } from 'rxjs';
+import { EMPTY } from 'rxjs';
 
-import { Product } from '../product';
 import { ProductService } from '../product.service';
+import { catchError } from 'rxjs/operators';
 
 @Component({
-  selector: 'pm-product-list',
-  templateUrl: './product-list-alt.component.html'
+    selector: 'pm-product-list',
+    templateUrl: './product-list-alt.component.html'
 })
-export class ProductListAltComponent implements OnInit, OnDestroy {
-  pageTitle = 'Products';
-  errorMessage = '';
-  selectedProductId;
-  
-  products: Product[] = [];
-  sub: Subscription;
+export class ProductListAltComponent  {
+    pageTitle = 'Products';
+    errorMessage = '';
+    selectedProductId;
 
-  constructor(private productService: ProductService) { }
+    products$ = this.productService.products$
+        .pipe(
+            catchError(err => {
+                this.errorMessage = err;
+                return EMPTY;
+            })
+        );
 
-  ngOnInit(): void {
-    this.sub = this.productService.getProducts().subscribe(
-      products => this.products = products,
-      error => this.errorMessage = error
-    );
-  }
+    constructor(private productService: ProductService) { }
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
-
-  onSelected(productId: number): void {
-    console.log('Not yet implemented');
-  }
+    onSelected(productId: number): void {
+        console.log('Not yet implemented');
+    }
 }
