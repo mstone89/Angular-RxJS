8f5ddeefd9fa5973f369773acce99e15b8906174
Molly Stone <molly.stone89@gmail.com>
2020-03-15 11:36:35 -0400

    use map operator to transform all products from http get

--- APM/src/app/products/product.service.ts
+++ APM/src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, tap, map } from 'rxjs/operators';
 
 import { Product } from './product';
 import { SupplierService } from '../suppliers/supplier.service';
@@ -16,6 +16,13 @@ export class ProductService {
 
     products$ = this.http.get<Product[]>(this.productsUrl)
                     .pipe(
+                        map(products => {
+                            return products.map(product => ({
+                                ...product,
+                                price: product.price * 1.5,
+                                searchKey: [product.productName]
+                            }) as Product);
+                        }),
                         tap(data => console.log('Products: ', JSON.stringify(data))),
                         catchError(this.handleError)
                     );
